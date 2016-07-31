7b1a05b5396198c8ca4aff099ca09ad764b5a355
xiaolun.xiang <xiaolun.xiang@langtaojin.com>
2016-07-31 12:39:39 +0800

    init project

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by apple on 16/7/31.
+//  Copyright © 2016年 LangTaoJin (Beijing) Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by apple on 16/7/31.
+//  Copyright © 2016年 LangTaoJin (Beijing) Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
